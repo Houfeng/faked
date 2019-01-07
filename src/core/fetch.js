cda851ab34d285369e366c8a61369373c0ea89d2
龙归 <longgui.wjb@alibaba-inc.com>
2019-01-07 15:36:55 +0800

    fix headers error while using originFetch

--- src/core/fetch.js
+++ src/core/fetch.js
@@ -1,9 +1,16 @@
 const faked = require('./faked');
 const Request = require('./request');
+const Headers = require('./headers');
 
 async function fetch(req, opts) {
   const response = await faked.handle(new Request(req, opts));
   if (response) return response;
+  if (opts.headers && opts.headers instanceof Headers) {
+    opts = {
+      ...opts,
+      headers: opts.headers.toMap()
+    };
+  }
   return window.originFetch(req, opts);
 }
 
